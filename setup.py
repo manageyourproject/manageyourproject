09dbb75c7e385990331789c45087fa865ea0e8a9
Aaron English <manageyourproject@protonmail.com>
2019-01-24 22:05:08 -0500

    testing version update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
         name='myp',
-        version='0.1.1',
+        version='0.1.2',
         author='Aaron English',
         author_email='manageyourproject@protonmail.com',
         license='GNU GPL-3',
